6766842ee7b3c528b927d333cdb0a0cb78c7d6dd
hoshi-3104 <mj67009@e.hiroshima-cu.ac.jp>
2024-09-20 18:02:41 +0900

    changing1-3

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,7 +1,21 @@
 import "./chatList.css"
+import React, { useState } from 'react';
 const ChatList = () => {
+  const [addMode, setAddMode]= useState(false)
     return (
-      <div className='chatList'>ChatList</div>
+      <div className='chatList'>
+        <div className="search">
+          <div className="searchBar">
+            <img src="./search.png" alt="" />
+            <input type="text" placeholder="Search" />
+          </div>
+          <img src={addMode ? "./minus.png": "./plus.png"} 
+          alt=""
+          className="add"
+          onClick={()=> setAddMode((prev) =>!prev)}
+            />
+        </div>
+      </div>
     )
   }
   
