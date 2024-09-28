915cd056e583430b935f9805456ea670af162486
hoshi-3104 <mj67009@e.hiroshima-cu.ac.jp>
2024-09-28 15:29:11 +0900

    changing1-4

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -15,6 +15,27 @@ const ChatList = () => {
           onClick={()=> setAddMode((prev) =>!prev)}
             />
         </div>
+        <div className="item">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Jane Doe</span>
+            <p>Hello</p>
+          </div>
+        </div>
+        <div className="item">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Jane Doe</span>
+            <p>Hello</p>
+          </div>
+        </div>
+        <div className="item">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Jane Doe</span>
+            <p>Hello</p>
+          </div>
+        </div>
       </div>
     )
   }
